1f553448464501bffbfcec8bc67a93697aeaca20
blacdav <davidaniefoik@gmail.com>
2024-07-17 13:55:02 +0100

    revert my last commit cos of an error

---
+++ app/home-page/notifications/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import NavigationBar from '../components/NavigationBar'
+
+const Page = () => {
+  return (
+    <div>
+      Notification Page
+      <NavigationBar />
+    </div>
+  )
+}
+
+export default Page
