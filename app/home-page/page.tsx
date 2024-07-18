0bf3b52147ee105055d905ea3033d362beecd383
Verifieddanny <dannyclassic56@gmail.com>
2024-07-18 17:28:31 +0100

    .

--- app/home-page/page.tsx
+++ app/home-page/page.tsx
@@ -1,24 +1,20 @@
-"use client"
-import React from 'react'
-import SearchBar from './components/SearchBar'
-import NavigationBar from './components/NavigationBar'
-import Poll from './components/Poll'
-import "./style.css"
+"use client";
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import NavigationBar from "./components/NavigationBar";
+import Poll from "./components/Poll";
+import "./style.css";
 
 const Page = () => {
   return (
     <main>
       <SearchBar />
-<<<<<<< HEAD
-      <div className='w-full px-5 h-full bg-slate-600 overflow-y-scroll mb-44 no-scroll'>
-=======
-      <div className='w-full h-full overflow-y-scroll bg-slate-100 mb-44'>
->>>>>>> a3d2ccbf4e6159a5d66db65dd4c02496f201b44c
+      <div className="w-full h-full overflow-y-scroll bg-slate-100 mb-44">
         <Poll />
       </div>
       <NavigationBar />
     </main>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
